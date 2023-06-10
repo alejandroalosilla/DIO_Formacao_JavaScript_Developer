b1fc580b30783f8ebdf9b8527700072c5142223d
Alejandro Alosilla <alejandroalosilla@gmail.com>
2023-06-10 00:51:32 -0300

    up1

---
+++ Aula001_Variaveis_e_Operadores/aula001.js
@@ -0,0 +1,7 @@
+let var1 = 10;
+const numero_pi = 3.14;
+let var2 = 10 * 10;
+
+console.log('Variável 1: ', var1);
+console.log('Constante: ', numero_pi);
+console.log('Variável 2: ', var2);
---
+++ Aula001_Variaveis_e_Operadores/desafio_aula001.js
@@ -0,0 +1,19 @@
+/*
+    Faça um programa para calcular o valor de uma viagem.
+
+    Você terá 3 variáveis. Sendo elas:
+    1 - Preço do combustível;
+    2 - Gasto médio de combustível do carro por KM;
+    3 - Distância em KM da viagem;
+
+    Imprima no console o valor que será gasto de combustível para
+    realizar esta viagem.
+*/
+
+let preco_do_combustivel = 5.79;
+let gasto_combustivel_km = 10;
+let distancia_km = 100;
+
+let valor_gasto_combustivel = (distancia_km / gasto_combustivel_km) * preco_do_combustivel;
+
+console.log(valor_gasto_combustivel + 'l');
