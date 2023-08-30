f426402544f3576500af3804c53624b71c2acce2
Alejandro Alosilla <alejandroalosilla@gmail.com>
2023-08-30 18:26:11 -0300

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,28 @@
-function incrementarJuros (valor, porcentagem) {
-    valor = valor + (porcentagem / 100) * valor;
-    return valor;
+function IMC (p, a) {
+    return p / Math.pow(a, 2);
 }
 
 
-let valor_final = incrementarJuros(100, 10);
+function analiseImc (imc) {
+    if (imc < 18.5) {
+        console.log('Abaixo do peso.');
+    } else if (imc >= 18.5 && imc <= 25) {
+        console.log('Peso normal.');
+    } else if (imc > 25 && imc <=30) {
+        console.log('Acima do peso.');
+    } else if (imc > 30 && imc <= 40) {
+        console.log('Obeso');
+    } else {
+        console.log('Obesidade Grave');
+    }
+}
 
-console.log(valor_final);
+
+(function () {
+    let peso = 140;
+    let altura = 1.78;
+
+    let valor_imc = IMC(peso, altura);
+    console.log('IMC:', valor_imc.toFixed(2));
+    analiseImc(valor_imc);
+})();
