8c814860b5963027b830295646e3b8e485ead26b
Alejandro Alosilla <alejandroalosilla@gmail.com>
2023-09-08 05:45:07 -0300

    Update aula004.js

--- Aula004_Objetos/aula004.js
+++ Aula004_Objetos/aula004.js
@@ -1,19 +1,22 @@
 class Pessoa {
     nome;
     idade;
+    ano_de_nascimento;
+
+    constructor (nome, idade) {
+        this.nome = nome;
+        this.idade = idade;
+        this.ano_de_nascimento = 2023 - idade;
+    }
 
     descrever() {
         console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade}`);
     }
 }
 
-const alejandro = new Pessoa();
-alejandro.nome = 'Alejandro';
-alejandro.idade = 24;
+const alejandro = new Pessoa('Alejandro', 25);
+const joao = new Pessoa('João', 26);
+const maria = new Pessoa('Maria', 18);
 
-const joao = new Pessoa();
-joao.nome = 'João';
-joao.idade = 23;
 
-alejandro.descrever();
-joao.descrever();
+console.log(alejandro);
