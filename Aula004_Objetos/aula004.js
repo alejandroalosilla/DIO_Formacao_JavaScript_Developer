ccb443a0dc93299167134af9c68e345d202c3dc4
Alejandro Alosilla <alejandroalosilla@gmail.com>
2023-09-08 19:53:58 -0300

    up

--- Aula004_Objetos/aula004.js
+++ Aula004_Objetos/aula004.js
@@ -14,9 +14,21 @@ class Pessoa {
     }
 }
 
-const alejandro = new Pessoa('Alejandro', 25);
+
+function compararPessoas(p1, p2) {
+    if (p1.idade > p2.idade) {
+        console.log(`A pessoa ${p1.nome} é mais velha que a pessoa ${p2.nome}!`);
+    } else if (p1.idade === p2.idade){
+        console.log(`A pessoa ${p1.nome} e ${p2.nome} tem a mesma idade!`);
+    } else {
+        console.log(`A pessoa ${p2.nome} é mais velha que a pessoa ${p1.nome}!`);
+    }
+}
+
+
+const alejandro = new Pessoa('Alejandro', 27);
 const joao = new Pessoa('João', 26);
 const maria = new Pessoa('Maria', 18);
 
-
+compararPessoas(alejandro, joao);
 console.log(alejandro);
--- test.js
+++ test.js
@@ -1,37 +1,39 @@
-const pessoa = {
-    nome: 'Alejandro',
-    idade: 25,
-    altura: 1.78,
-    descrever: function() {
-        console.log(`Meu nome é ${this.nome}, minha idade é ${this.idade} e minha altura é ${this.altura}`);
-    }
-};
-
-pessoa.descrever();
-
-
 class carro {
-    nome;
     ano;
+    modelo;
     preco;
-    placa;
 
-    descrever (){
-        console.log(`O nome do carro é ${this.nome}, o ano é ${this.ano}, o preço é ${this.preco} e a placa é ${this.placa}`)
+    constructor (ano, modelo, preco) {
+        this.ano = ano;
+        this.modelo = modelo;
+        this.preco = preco;
+    }
+
+    descreverCarro (){
+        console.log(`
+        Dados do carro:
+        ----------------
+        ano: ${this.ano}
+        modelo: ${this.modelo}
+        preço: ${this.preco}
+        `)
     }
 }
 
-let honda = new carro()
-honda.nome = 'Honda Fit'
-honda.ano = 2023
-honda.preco = 60000
-honda.placa = 'PG567'
+function compararCarros (p1, p2){
+    if (p1.preco > p2.preco){
+        console.log(`O carro ${p1.modelo} é mais caro que o carro ${p2.modelo}!`);
+    } else if (p1.preco < p2.preco) {
+        console.log(`O carro ${p2.modelo} é mais caro que o carro ${p1.modelo}!`);
+    } else {
+        console.log(`Os carros ${p1.modelo} e ${p2.modelo} tem o mesmo preço de ${p1.preco} reais!`)
+    }
+}
 
-let toyota = new carro()
-toyota.nome = 'Toyota Etios'
-toyota.ano = 2017
-toyota.preco = 40000
-toyota.placa = 'ASD3134'
 
-honda.descrever()
-toyota.descrever()
+let carro1 = new carro(2017, 'Honda Fit', 60000);
+let carro2 = new carro(2023, 'Honda City', 150000);
+carro1.descreverCarro();
+carro2.descreverCarro();
+
+compararCarros(carro1, carro2);
